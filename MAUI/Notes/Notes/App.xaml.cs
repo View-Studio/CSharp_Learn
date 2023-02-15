fc807d1e2dd882811b5c6bd51d0079be2a74f0e7
Rudals <58895477+View-Studio@users.noreply.github.com>
2023-02-15 16:33:26 +0900

    Template 코드 주석 추가
    Template 코드 주석 추가

--- MAUI/Notes/Notes/App.xaml.cs
+++ MAUI/Notes/Notes/App.xaml.cs
@@ -6,6 +6,6 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		MainPage = new AppShell(); // MainPage의 마크업의 루트 태그가 굳이 Shell 이 아니어도 된다.
 	}
 }
--- MAUI/Notes/Notes/MauiProgram.cs
+++ MAUI/Notes/Notes/MauiProgram.cs
@@ -2,17 +2,18 @@
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
+	public static MauiApp CreateMauiApp() // EntryPoint
 	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+		var builder = MauiApp.CreateBuilder(); // MauiAppBuilder 초기화
+
+        builder
+			.UseMauiApp<App>()  // <> 괄호 안에 들어간 형식의 Application 을 사용한다
+            .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
-		return builder.Build();
+		return builder.Build(); // MauiApp을 빌드한다.
 	}
 }
