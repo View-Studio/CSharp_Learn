a01aedcfd379fd232239266d59ffd9cb4bc8531a
Rudals <58895477+View-Studio@users.noreply.github.com>
2023-02-16 15:37:45 +0900

    AllNotes 주석 추가
    AllNotes 주석 추가

--- MAUI/Notes/Notes/Models/AllNotes.cs
+++ MAUI/Notes/Notes/Models/AllNotes.cs
@@ -4,7 +4,7 @@ namespace Notes.Models;
 
 internal class AllNotes
 {
-    public ObservableCollection<Note> Notes { get; set;} = new ObservableCollection<Note>();
+    public ObservableCollection<Note> Notes { get; set; } = new ObservableCollection<Note>();
 
     public AllNotes() =>
         LoadNotes();
@@ -13,19 +13,28 @@ internal class AllNotes
     {
         Notes.Clear();
 
+        // Get the folder where the notes are stored.
         string appDataPath = FileSystem.AppDataDirectory;
 
+        // Use Linq extensions to load the *.notes.txt files.
         IEnumerable<Note> notes = Directory
-            .EnumerateFiles(appDataPath, "*.notes.txt")
-            .Select(filename => new Note()
-            {
-                Filename = filename,
-                Text = File.ReadAllText(filename),
-                Date = File.GetCreationTime(filename)
-            })
-            .OrderBy(note => note.Date);
 
-        foreach (Note note in notes) 
+                                    // Select the file names from the directory
+                                    .EnumerateFiles(appDataPath, "*.notes.txt")
+
+                                    // Each file name is used to create a new Note
+                                    .Select(filename => new Note()
+                                    {
+                                        Filename = filename,
+                                        Text = File.ReadAllText(filename),
+                                        Date = File.GetCreationTime(filename)
+                                    })
+
+                                    // With the final collection of notes, order them by date
+                                    .OrderBy(note => note.Date);
+
+        // Add each note into the ObservableCollection
+        foreach (Note note in notes)
             Notes.Add(note);
     }
 }
