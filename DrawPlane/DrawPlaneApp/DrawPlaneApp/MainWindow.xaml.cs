7039ec77b49a066b6ce2f6bd609716a14518bbae
Rudals <58895477+View-Studio@users.noreply.github.com>
2023-02-21 17:08:42 +0900

    그림판 테스트 프로젝트 MVVM 아키텍쳐로 리팩터링
    그림판 테스트 프로젝트 MVVM 아키텍쳐로 리팩터링

--- DrawPlane/DrawPlaneApp/DrawPlaneApp/MainWindow.xaml.cs
+++ DrawPlane/DrawPlaneApp/DrawPlaneApp/MainWindow.xaml.cs
@@ -8,21 +8,34 @@ namespace DrawPlaneApp
 {
     public partial class MainWindow : Window
     {
-        public bool mClicked = false;
-        public bool lineClicked = false;
+        public bool mClicked;
+        public bool lineClicked;
+        public bool rectangleClicked;
+        public bool eraseClicked;
+        public bool circleClicked;
         public Point prePosition;
-        Line line;
-        Brush myBrush = new SolidColorBrush(Colors.White);
+        public Line line;
+        public Rectangle temprectangle;
+        public Rectangle myrectangle;
+        public Ellipse myellipse;
+        public Ellipse tempellipse;
+        public Brush myBrush; 
 
         public MainWindow()
         {
             InitializeComponent();
+            mClicked = false;
+            lineClicked = true;
+            rectangleClicked = false;
+            circleClicked = false;
+            eraseClicked = false;
+            myBrush = new SolidColorBrush(Colors.Black);
         }
 
         private void canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             mClicked = true;
-            prePosition = e.GetPosition(canvas);
+            prePosition = e.GetPosition(canvas); // 지정된 요소를 기준으로 마우스 포인터의 상대적인 위치를 반환
         }
 
         private void canvas_MouseMove(object sender, MouseEventArgs e)
@@ -30,22 +43,154 @@ namespace DrawPlaneApp
             var nowPosition = e.GetPosition(canvas);
             if (mClicked)
             {
-                line = new Line();
-                line.X1 = prePosition.X;
-                line.X2 = nowPosition.X;
-                line.Y1 = prePosition.Y;
-                line.Y2 = nowPosition.Y;
-                line.Stroke = myBrush;
-                line.StrokeThickness = 2;
-                canvas.Children.Add(line);
+                if (lineClicked)
+                {
+                    line = new Line();
+                    line.X1 = prePosition.X;
+                    line.X2 = nowPosition.X;
+                    line.Y1 = prePosition.Y;
+                    line.Y2 = nowPosition.Y;
+                    line.Stroke = myBrush;
+                    line.StrokeThickness = 2;
+                    canvas.Children.Add(line);
 
-                prePosition = nowPosition;
+                    prePosition = nowPosition;
+                }
+
+                // 사각형그리기
+                else if (rectangleClicked)
+                {
+                    if (e.MouseDevice.LeftButton == MouseButtonState.Pressed)
+                    {
+                        myrectangle = new Rectangle();
+
+                        myrectangle.Stroke = Brushes.Plum;
+                        myrectangle.Fill = myBrush;
+                        myrectangle.Opacity = 0.8;
+
+                        double left = prePosition.X;
+                        double top = prePosition.Y;
+
+                        double width = nowPosition.X - prePosition.X;
+                        double height = nowPosition.Y - prePosition.Y;
+
+                        if (nowPosition.X < prePosition.X)
+                        {
+                            left = nowPosition.X;
+                            width *= -1;
+                        }
+                        if (nowPosition.Y < prePosition.Y)
+                        {
+                            top = nowPosition.Y;
+                            height *= -1;
+                        }
+
+                        myrectangle.Margin = new Thickness(left, top, 0, 0);
+                        myrectangle.Width = width;
+                        myrectangle.Height = height;
+
+                        canvas.Children.Remove(temprectangle);
+                        temprectangle = myrectangle;
+                        canvas.Children.Add(myrectangle);
+                    }
+                    else
+                    {
+                        temprectangle = null;
+                    }
+                }
+
+                else if (circleClicked)
+                {
+                    if (e.MouseDevice.LeftButton == MouseButtonState.Pressed)
+                    {
+                        myellipse = new Ellipse();
+
+                        myellipse.Fill = myBrush;
+                        myellipse.StrokeThickness = 2;
+                        myellipse.Stroke = Brushes.LightSkyBlue;
+                        myellipse.Opacity = 0.8;
+
+                        double width = nowPosition.X - prePosition.X;
+                        double height = nowPosition.Y - prePosition.Y;
+
+                        double left = prePosition.X;
+                        double top = prePosition.Y;
+
+                        if (nowPosition.X < prePosition.X)
+                        {
+                            left = nowPosition.X;
+                            width *= -1;
+                        }
+                        if (nowPosition.Y < prePosition.Y)
+                        {
+                            top = nowPosition.Y;
+                            height *= -1;
+                        }
+
+                        myellipse.Margin = new Thickness(left, top, 0, 0);
+                        myellipse.Width = width;
+                        myellipse.Height = height;
+
+                        canvas.Children.Remove(tempellipse);
+                        tempellipse = myellipse;
+                        canvas.Children.Add(myellipse);
+                    }
+                    else
+                    {
+                        tempellipse = null;
+                    }
+                        
+                }
+
+
+                else if (eraseClicked)
+                {
+                    tempellipse = new Ellipse();
+
+                    if (e.MouseDevice.LeftButton == MouseButtonState.Pressed)
+                    {
+                        myellipse = new Ellipse();
+                        myellipse.Fill = new SolidColorBrush(Colors.White); 
+                        myellipse.StrokeThickness = 2;
+                        myellipse.Opacity = 1.0;
+
+                        double width = nowPosition.X - prePosition.X;
+                        double height = nowPosition.Y - prePosition.Y;
+
+                        double left = prePosition.X;
+                        double top = prePosition.Y;
+
+                        if (nowPosition.X < prePosition.X)
+                        {
+                            left = nowPosition.X;
+                            width *= -1;
+                        }
+                        if (nowPosition.Y < prePosition.Y)
+                        {
+                            top = nowPosition.Y;
+                            height *= -1;
+                        }
+
+                        myellipse.Margin = new Thickness(left, top, 0, 0);
+                        myellipse.Width = width;
+                        myellipse.Height = height;
+
+                        canvas.Children.Remove(tempellipse);
+                        tempellipse = myellipse;
+                        canvas.Children.Add(myellipse);
+                    }
+                    else
+                        tempellipse = null;
+                }
+
             }
         }
 
         private void canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             mClicked = false;
+            temprectangle = null;
+            tempellipse = null;
         }
 
         private void canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
@@ -60,22 +205,34 @@ namespace DrawPlaneApp
 
         private void button_line_Click(object sender, RoutedEventArgs e)
         {
-
+            lineClicked = true;
+            rectangleClicked = false;
+            eraseClicked = false;
+            circleClicked = false;
         }
 
         private void button_circle_Click(object sender, RoutedEventArgs e)
         {
-
+            lineClicked = false;
+            rectangleClicked = false;
+            eraseClicked = false;
+            circleClicked = true;
         }
 
         private void button_rectangle_Click(object sender, RoutedEventArgs e)
         {
-
+            lineClicked = false;
+            rectangleClicked = true;
+            eraseClicked = false;
+            circleClicked = false;
         }
 
         private void button_erase_Click(object sender, RoutedEventArgs e)
         {
-
+            lineClicked = false;
+            rectangleClicked = false;
+            eraseClicked = true;
+            circleClicked = false;
         }
 
         private void button_fileopen_Click(object sender, RoutedEventArgs e)
@@ -100,7 +257,8 @@ namespace DrawPlaneApp
 
         private void button_allerase_Click(object sender, RoutedEventArgs e)
         {
-
+            canvas.Children.Clear();
+            canvas.Background = Brushes.Transparent;
         }
 
         private void color_black_Click(object sender, RoutedEventArgs e)
---
+++ DrawPlane/DrawPlaneApp/DrawPlaneApp/Views/DrawPlane.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace DrawPlaneApp.Views;
+
+public partial class DrawPlane : Window
+{
+    private bool V_LeftMouseClick;
+    private E_Click V_ClickState;
+    private E_Color V_ColorState;
+    private E_Mouse V_MouseState;
+    private Point V_PrePosition;
+    public DrawPlane()
+    {
+        InitializeComponent();
+        V_LeftMouseClick = false;
+        V_ClickState = E_Click.Line;
+        V_ColorState = E_Color.Black;
+        V_MouseState = E_Mouse.Normal;
+    }
+
+    private void e_canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) // 좌클릭
+    {
+        V_LeftMouseClick = true;
+        V_PrePosition = e.GetPosition(canvas);   
+    }
+
+    private void e_canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) // 좌클릭 해제
+    {
+        V_LeftMouseClick = false;
+    }
+
+    private void e_canvas_MouseMove(object sender, MouseEventArgs e) // 마우스 무브
+    {
+        Point nowPosition = e.GetPosition(canvas);
+
+        if (V_LeftMouseClick)
+        {
+            switch (V_MouseState)
+            {
+                case E_Mouse.Normal:
+                    break;
+                case E_Mouse.Grap:
+                    break;
+                case E_Mouse.Draw:
+                    Draw();
+                    break;
+            }
+        }
+    }
+
+    private void e_canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e) // 우클릭
+    {
+
+    }
+
+    private void e_canvas_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+    {
+
+    }
+
+    private void e_colorChange(object sender, RoutedEventArgs e)
+    {
+        Button button = (Button)e.Source;
+        string newColor = button.Name;
+        string[] colorNames = Enum.GetNames(typeof(E_Color));
+
+        for (int i = 0; i < colorNames.Length; ++i)
+        {
+            if (newColor == colorNames[i])
+            {
+                V_ColorState = (E_Color)i;
+            }
+        }
+    }
+    private void e_shapeChange(object sender, RoutedEventArgs e)
+    {
+        Button button = (Button)e.Source;
+        string newShape = button.Name;
+        string[] shapeNames = Enum.GetNames(typeof(E_Click));
+
+        for (int i = 0; i < shapeNames.Length; ++i)
+        {
+            if (newShape == shapeNames[i])
+            {
+                V_ClickState = (E_Click)i;
+            }
+        }
+    }
+
+    private void Draw()
+    {
+        switch (V_ColorState) { }
+    }
+
+    private void CircleDraw()
+    {
+
+    }
+}
+
+enum E_Mouse
+{
+    Normal = 0,
+    Draw,
+    Grap
+}
+enum E_Click
+{
+    Line = 0,
+    Circle,
+    Rectangle,
+    Erase
+}
+enum E_Color
+{
+    Black = 0,
+    Red,
+    Green,
+    Blue,
+    Yellow
+}
